fe57bf7d0206f6337a4376398f52abb1cda9a077
Selim Waly <selimwalyy@gmail.com>
2024-07-30 16:20:25 +0300

    Implement Splash Screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:books_app/screens/home_screen.dart';
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen({super.key});
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 1, milliseconds: 500),
+          () => Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomeScreen(),
+            ),
+          ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Books App',
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
   }
 }
