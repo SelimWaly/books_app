bac5380fe28c88def6348a409da976bb1b0483b3
Selim Waly <selimwalyy@gmail.com>
2024-08-02 19:02:47 +0300

    Replicated programming screen with accountable changes

---
+++ lib/screens/categories/science_screen.dart
@@ -0,0 +1,96 @@
+import 'package:books_app/screens/book_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:books_app/env/theme.dart';
+import 'package:books_app/logic/book_cubit.dart';
+import 'package:books_app/widgets/book_widget.dart';
+import 'package:books_app/widgets/book_card_widget.dart';
+
+class ScienceScreen extends StatelessWidget {
+  const ScienceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BookCubit, BookState>(
+      builder: (context, state) {
+        var cubit = BookCubit.get(context);
+
+        if (state is Loading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is Success) {
+          if (cubit.scienceBooks.isEmpty) {
+            return const Center(child: Text("No science books were found."));
+          }
+        }
+        return DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            backgroundColor: Colors.transparent,
+            appBar: AppBar(
+              backgroundColor: Colors.white,
+              elevation: 0,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Colors.black),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              title: const Text(
+                "Science Books",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+            body: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                      left: 15, right: 15, top: 10, bottom: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.shade200,
+                    boxShadow: [
+                      BoxShadow(
+                          color: shadowColor.withOpacity(0.1),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: const Offset(1, 1)
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: TabBarView(
+                    children: [
+                      ListView.separated(
+                        scrollDirection: Axis.vertical,
+                        padding: const EdgeInsets.only(left: 15, right: 15),
+                        itemCount: cubit.scienceBooks.length,
+                        itemBuilder: (context, index) {
+                          var scienceBook = cubit.scienceBooks[index];
+                          return InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => BookScreen(book: scienceBook),
+                                ),
+                              );
+                            },
+                            child: BookItem(book: scienceBook),
+                          );
+                        },
+                        separatorBuilder: (context, index) {
+                          return const SizedBox(width: 10);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
