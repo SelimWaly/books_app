3a3b95dc16ed5ce39236b35e6bb1b1c38d4eae49
Selim Waly <selimwalyy@gmail.com>
2024-08-02 16:34:33 +0300

    Await response from Google Books API

--- lib/logic/book_cubit.dart
+++ lib/logic/book_cubit.dart
@@ -27,7 +27,7 @@ class BookCubit extends Cubit<BookState> {
   getProgrammingBooks() async{
     try {
       emit(Loading());
-      final response = http.get(Uri.parse('$apiUrl?q=programming'));
+      final response = await http.get(Uri.parse('$apiUrl?q=programming'));
       if (response.statusCode == 200) {
         var results = json.decode(response.body);
         debugPrint('Results: $results');
