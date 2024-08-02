387d130f8bacb11d75e3bfc3324a86a09bec9d48
Selim Waly <selimwalyy@gmail.com>
2024-08-02 20:34:50 +0300

    Resolve warnings by setting constants to constructors to improve performance

--- lib/widgets/book_card_widget.dart
+++ lib/widgets/book_card_widget.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:books_app/widgets/styled_image_widget.dart';
 
 class BookCard extends StatelessWidget {
-  BookCard({ Key? key, required this.book}) : super(key: key);
+  const BookCard({ super.key, required this.book});
   final BookModel book;
 
   @override
@@ -13,8 +13,8 @@ class BookCard extends StatelessWidget {
     return Container(
       width: 200,
       height: 255,
-      margin: EdgeInsets.only(right: 25),
-      padding: EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 25),
+      margin: const EdgeInsets.only(right: 25),
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 20, bottom: 25),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(20),
@@ -23,20 +23,20 @@ class BookCard extends StatelessWidget {
             color: shadowColor.withOpacity(0.1),
             spreadRadius: 1,
             blurRadius: 1,
-            offset: Offset(1, 1), // changes position of shadow
+            offset: const Offset(1, 1),
           ),
         ],
       ),
       child: Column(
         children: [
           Container(
-            padding: EdgeInsets.only(left: 5),
+            padding: const EdgeInsets.only(left: 5),
             decoration: BoxDecoration(
               color: Color(Random().nextInt(0xffffffff)).withAlpha(0xff),
               borderRadius: BorderRadius.circular(12),
             ),
             child: Container(
-              padding: EdgeInsets.all(15),
+              padding: const EdgeInsets.all(15),
               decoration: BoxDecoration(
                 color: primary,
                 borderRadius: BorderRadius.circular(7),
@@ -46,17 +46,17 @@ class BookCard extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(height: 15,),
+          const SizedBox(height: 15,),
           Text(book.title ?? '',
               maxLines: 2, overflow: TextOverflow.ellipsis, textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 16, color: primary ,fontWeight: FontWeight.w600)
+              style: const TextStyle(fontSize: 16, color: primary ,fontWeight: FontWeight.w600)
           ),
-          SizedBox(height: 8,),
+          const SizedBox(height: 8,),
           RichText(
               textAlign: TextAlign.center,
               text: TextSpan(
                   children: [
-                    TextSpan(text: book.price ?? '', style: TextStyle(fontSize: 16, color: primary ,fontWeight: FontWeight.w500))
+                    TextSpan(text: book.price ?? '', style: const TextStyle(fontSize: 16, color: primary ,fontWeight: FontWeight.w500))
                   ]
               )
           )
