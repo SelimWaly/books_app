fe7992ba280f10462a3db147ff14625339d170d4
Selim Waly <selimwalyy@gmail.com>
2024-08-03 18:17:38 +0300

    Fix nullable/non existent values in model

--- lib/model/book_model.dart
+++ lib/model/book_model.dart
@@ -5,19 +5,19 @@ class BookModel {
   String? publishedDate;
   String? author;
   String? saleability;
-  String? price;
+  double? price;
   String? buyLink;
   String? readLink;
 
   BookModel.fromJson(Map<String,dynamic> json){
-    title = json['volumeInfo']['title'];
-    image = json['volumeInfo']['imageLinks']['thumbnail'];
-    description = json['volumeInfo']['description'];
-    publishedDate = json['volumeInfo']['publishedDate'];
-    author = json['volumeInfo']['authors'][0]; /// Only show first author
-    saleability = json['saleInfo']['saleability'];
-    price = json['saleInfo']['listPrice']['amount'];
-    buyLink = json['saleInfo']['buyLink'];
-    readLink = json['accessInfo']['webReaderLink'];
+    title = json['volumeInfo']['title'] ?? 'No title';
+    image = json['volumeInfo']['imageLinks']['thumbnail'] ?? 'https://github.com/SelimWaly/drive-temp-1/blob/main/unknown.png?raw=true';
+    description = json['volumeInfo']['description'] ?? 'No description';
+    publishedDate = json['volumeInfo']['publishedDate'] ?? 'No publish date';
+    author = json['volumeInfo']['authors'] != null? json['volumeInfo']['authors'][0]: 'No author'; /// Only show first author
+    saleability = json['saleInfo']['saleability'] ?? 'NOT_FOR_SALE';
+    price = json['saleInfo']['listPrice'] != null? json['saleInfo']['listPrice']['amount']: 0.0;
+    buyLink = json['saleInfo']['buyLink'] ?? json['accessInfo']['webReaderLink'];
+    readLink = json['accessInfo']['webReaderLink'] ?? '';
   }
 }
